acfd637aa24a752b8edc7aef8fec8b6b737713ce
Jankish <danieljankovic89@gmail.com>
2015-07-26 04:17:41 +0200

    going on break

--- matlab/simple.m
+++ matlab/simple.m
@@ -13,21 +13,23 @@ fileName2=('noise_signals/ventilation_8khz.wav');
 LOrig1=length(xOrig1);
 LOrig2=length(xOrig2);
 xLen=min(LOrig1);
-xLen = min(LOrig1,LOrig2); % xLen ?r nu den kortaste l?ngden
-Nstrength = 10; % Styr hur stark st?rningen ska vara
+xLen = min(LOrig1,LOrig2); % xLen is the shortest length
+Nstrength = 10; % Controls the strength of the noise added to the signal
 disp('Adderad brusstyrka: '),disp(Nstrength)
 x=xOrig1(1:xLen)+Nstrength*xOrig2(1:xLen);
 % x=xOrig1(1:xLen);
 
+% Create a buffer with 40 samples in each frame
 blocklength=40;
-
 buff = buffer(x,blocklength);
-
 buffSquared = buff.^2;
 
+% Calculate the average power for each frame
 for i=1:length(buffSquared)
     Frameaverage(i,1)=sum(buffSquared(1:end,i))/blocklength;
 end
+
+
 figure(1)
 subplot(211)
 plot(x(1:xLen)), title('Babyljud')
