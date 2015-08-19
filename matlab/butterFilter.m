8bcd6290bdc5f4c050ca75f2b006b3a40131b46d
Jankish <danieljankovic89@gmail.com>
2015-08-19 12:22:41 +0200

    zip file added

--- matlab/butterFilter.m
+++ matlab/butterFilter.m
@@ -1,12 +1,13 @@
 % Values for the bandpass 
-A_stop1 = 60;
+A_stop1 = 100;
 F_stop1 = 200;
 F_pass1 = 300;
 F_pass2 = 1300;
 F_stop2 = 1400;
-A_stop2 = 60;
+A_stop2 = 100;
 A_pass = 1;
 Fs = 8000;
+Filtering = 1;
 
 % Design and create Bandpass filter
 BandPassSpecObj = fdesign.bandpass(F_stop1, F_pass1, ...
@@ -15,5 +16,5 @@ BandPassFilt = design(BandPassSpecObj,'butter');
 
 % Graphical representaion of the bandpass filter 
 % Uncomment for usage, requires DSP toolbox for MATLAB
-%  fvtool(BandPassFilt)
+% fvtool(BandPassFilt)
 
